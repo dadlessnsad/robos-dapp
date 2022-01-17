4f180fc1a9bcf39930588f9c2ff65ff503b44fb0
dadlessnsad <tbodnar85@gmail.com>
2022-01-17 11:58:53 -0700

    Manufacture

--- packages/react-app/src/components/manufacture/Manufacture.js
+++ packages/react-app/src/components/manufacture/Manufacture.js
@@ -1,13 +1,31 @@
 import './Manufacture.css';
 import React from "react";
 
+import RoboTwo from "../../images/GenesisRobos/5.png";
+import RoboOne from "../../images/GenesisRobos/7.png";
+
 function Manufacture() {
     return (
         <div className="Manufacture" id="Manufacture">
-            <div className="ManufactureBox">
+            {/* <div className="ManufactureBox">
                 <h1 className="ManufactureTitle">Manufacturing Coming Soon!</h1>
+            </div> */}
+            <div className="ManufactureCtn">
+                <div className="ManufactureBox1">
+                    <h1>Robo 1</h1>
+                    <img className="UserRobo" src={RoboOne} alt="robo" />
+                </div>
+                <div className="BtnCTN">
+                    <button className="ManufactureBtn">Manufacture</button>
+                </div>
+                <div className="ManufactureBox2">
+                    <h1>Robo 2</h1>
+                    <img className="UserRobo" src={RoboTwo} alt="robo" />
+                </div>
             </div>
+            
         </div>
+        
     )
 }
 
